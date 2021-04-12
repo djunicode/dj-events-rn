d546e199984c1660167f54d3196bc0bfe865d30d
bhsrampage <burhanuddinsavli@gmail.com>
2021-04-12 17:26:30 +0530

    com card fixed

--- src/components/CommitteeScreen/AboutComponent.js
+++ src/components/CommitteeScreen/AboutComponent.js
@@ -1,37 +1,75 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
-import {View, StyleSheet, Image, Text} from 'react-native';
-import {backDropColor, subtextColor} from '../../Constants';
+import React, {useState} from 'react';
+import {View, StyleSheet, Image, Text, ScrollView} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {backDropColor, height, subtextColor} from '../../Constants';
 
 const logo = require('../../images/acm_logo.png');
 
 const About = (props) => {
+  const [isBig, setIsBig] = useState(false);
+
+  const onClick = () => {
+    isBig ? setIsBig(false) : setIsBig(true);
+  };
+
   return (
-    <View style={styles.container}>
-      <Image source={logo} style={styles.image} />
-      <View style={{paddingLeft: 10, paddingRight: 20}}>
-        <Text style={styles.title}>About Us</Text>
-        <View style={{flexShrink: 1}}>
-          <Text style={styles.text}>
-          It is a long established fact that a reader will be distracted 
-            {/* {props.about} */}
-          </Text>
+    <TouchableOpacity onPress={onClick}>
+      <View
+        style={[
+          styles.container,
+          {
+            height: height * (isBig ? 0.65 : 0.25),
+            flexDirection: isBig ? 'column' : 'row',
+          },
+        ]}>
+        <Image
+          source={logo}
+          style={
+            isBig
+              ? {
+                  marginTop: 5,
+                  width: '20%',
+                  height: '20%',
+                  borderRadius: 15,
+                  alignSelf: 'center',
+                }
+              : styles.image
+          }
+        />
+        <View style={{paddingLeft: 10, paddingRight: 20}}>
+          <Text style={styles.title}>About Us</Text>
+          <View>
+            {isBig ? (
+              <Text
+                style={[styles.text, {textAlign: 'center'}]}
+                ellipsizeMode="tail">
+                {props.about}
+              </Text>
+            ) : (
+              <Text
+                style={[styles.text, {marginRight: 100}]}
+                ellipsizeMode="tail"
+                numberOfLines={5}>
+                {props.about}
+              </Text>
+            )}
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
     backgroundColor: backDropColor,
     margin: 15,
     borderRadius: 15,
-    height: '20%',
+    width: '95%',
   },
   image: {
-    width: '35%',
+    width: '33%',
     height: '100%',
     borderBottomLeftRadius: 15,
     borderTopLeftRadius: 15,
@@ -45,9 +83,9 @@ const styles = StyleSheet.create({
   text: {
     fontFamily: 'OpenSans-Regular',
     color: 'rgba(255,255,255,0.84)',
-    marginRight: 170,
     paddingTop: 10,
-    paddingBottom: 15,
+    paddingBottom: 10,
+    fontSize: 16,
   },
 });
 
